59d3292f86541f6386807670a5f69819c023e7f4
valerievichmyhai <91340890+mykhailooprelyansky@users.noreply.github.com>
2021-12-08 22:12:25 +0200

    responsive header

--- js/modal.js
+++ js/modal.js
@@ -19,17 +19,15 @@
 
     e.currentTarget.reset();
   });
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+
+    menuBtnRef.classList.toggle('is-open');
+    menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileMenuRef.classList.toggle('is-open');
+  });
 })();
-
-//  <script src="./js/modal.js"></script>
-//     <script>
-//       (() => {
-//         document.querySelector('.js-speaker-form').addEventListener('submit', e => {
-//           e.preventDefault();
-
-//           new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
-
-//           e.currentTarget.reset();
-//         });
-//       })();
-//     </script>
