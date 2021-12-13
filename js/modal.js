7c37a08c83d366192db131b155f176281c604d6a
valerievichmyhai <91340890+mykhailooprelyansky@users.noreply.github.com>
2021-12-13 23:16:11 +0200

    finished responsive index.html

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,27 @@
 (() => {
   const refs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menu: document.querySelector('[data-menu]'),
+  };
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+  }
+  const wqad = {
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
+  wqad.openModalBtn.addEventListener('click', toggleModal);
+  wqad.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
+    wqad.modal.classList.toggle('is-hidden');
   }
 
   document.querySelector('.js-speaker-form').addEventListener('submit', e => {
@@ -18,16 +30,5 @@
     new FormData(e.currentTarget).forEach((value, name) => console.log(`${name}: ${value}`));
 
     e.currentTarget.reset();
-  });
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
-
-  menuBtnRef.addEventListener('click', () => {
-    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
-
-    menuBtnRef.classList.toggle('is-open');
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
-
-    mobileMenuRef.classList.toggle('is-open');
   });
 })();
